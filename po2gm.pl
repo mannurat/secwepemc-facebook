64733577de7d99acf7f2bc1632f329371cf5e710
Kevin Scannell <kscanne@gmail.com>
2012-02-20 17:05:31 -0600

    More sophisticated handling of variables, allows dates to be processed correctly

--- po2gm.pl
+++ po2gm.pl
@@ -21,27 +21,73 @@ if (scalar @ARGV != 1) {
 	die "Usage: $0 POFILE\n";
 }
 
+sub insert_backref {
+	(my $str, my $var, my $backref) = @_;
+	$str =~ s/^$var/\$$backref"+"/;
+	$str =~ s/$var$/"+"\$$backref/;
+	$str =~ s/$var/"+"\$$backref"+"/;
+	return $str;
+}
+
+sub insert_all_backrefs {
+	(my $orig, my $str) = @_;
+	my $vars = 1;
+	while ($orig =~ m/(%[A-Za-z])/g) {
+		$vars++;
+		$str = insert_backref($str, $1, $vars);
+	}
+	return $str;
+}
+
 my @outputlines;
+my %formats;
+$formats{'%A'} = '%A'; # simple days of the week: "Saturday"
+$formats{'%B'} = '%B'; # simple month names: "February"
 
 my $aref = Locale::PO->load_file_asarray($ARGV[0]);
 foreach my $msg (@$aref) {
+	next if $msg->fuzzy();
 	my $id = $msg->msgid();
 	my $str = $msg->msgstr();
+	my $note = $msg->automatic();
 	if ($str and $id and $id ne '""') {
 		my $tempid = $msg->dequote($id);
 		my $tempstr = $msg->dequote($str);
 		if ($tempstr ne '') {
-			$tempid =~ s/'/\\'/g;
-			$tempid =~ s/([.])/\\$1/g;
-			$tempid =~ s/([()?])/\\\\$1/g;
-			$tempid =~ s/%d/([0-9]+)/;
-			$tempid =~ s/%s/(<a [^>]+>[^<]+<\/a>)/;
-			$tempstr =~ s/"/\\"/g;
-			$tempstr =~ s/^%[ds]/\$2"+"/;
-			$tempstr =~ s/%[ds]$/"+"\$2/;
-			$tempstr =~ s/%[ds]/"+"\$2"+"/;
-			$tempid = '(^|="|>)'.$tempid.'(?=($|"|<))';
-			push @outputlines, "  d = r(d, '$tempid', \"\$1\"+\"$tempstr\");\n";
+			if (defined($note) and $note =~ /Format\./) {
+				$formats{$tempid} = $tempstr;
+			}
+			elsif (defined($note) and $note =~ /(Day of the Week|Month Name)/) {
+				for my $fmt (keys %formats) {
+					if (($fmt =~ /%A/ and $note =~ /Day of the Week/) or
+					    ($fmt =~ /%B/ and $note =~ /Month Name/)) {
+						my $regex = $fmt;  # e.g. "%B %d"
+						$regex =~ s/%[AB]/$tempid/;
+						my $orig = $regex;
+						$regex =~ s/%d/([0-9]{1,2})/;  # Now "February ([0-9]{1,2})
+						$regex =~ s/%Y/([0-9]{4})/;
+						$regex =~ s/%s/([0-9:apm]+)/;
+						$regex = '(^|="|>)'.$regex.'(?=($|"|<))';
+						my $repl = $formats{$fmt};        # e.g. "%d %B"
+						$repl =~ s/%[AB]/$tempstr/;       # Now "%d Feabhra"
+						$repl = insert_all_backrefs($orig, $repl);
+						push @outputlines, "  d = r(d, '$regex', \"\$1\"+\"$repl\");\n";
+					}
+				}
+			}
+			else {
+				$tempid =~ s/'/\\'/g;
+				$tempid =~ s/([.])/\\$1/g;
+				$tempid =~ s/([()?])/\\\\$1/g;
+				my $orig = $tempid;
+				$tempid =~ s/%a/(<a [^>]+>[^<]+<\/a>)/g;
+				$tempid =~ s/%d/([0-9]+)/g;
+				$tempid =~ s/%s/([^<" ]+)/g;
+				$tempstr =~ s/"/\\"/g;
+				$tempstr = insert_all_backrefs($orig, $tempstr);
+				$tempid = '(^|="|>)'.$tempid.'(?=($|"|<))';
+				push @outputlines, "  d = r(d, '$tempid', \"\$1\"+\"$tempstr\");\n";
+			}
 		}
 	}
 }
