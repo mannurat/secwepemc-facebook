6ac4ceffdfca36462444b9fcd466a499d43d078e
Kevin Scannell <kscanne@gmail.com>
2012-05-03 21:22:35 -0500

    more portable call to mktemp (Mac OS X)

--- generate-gm.sh
+++ generate-gm.sh
@@ -14,7 +14,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-TEMPFILE=`mktemp`
+TEMPFILE=`mktemp /tmp/tmp.XXXXXXXXXX`
 cat LINGUAS | tr "\t" "~" |
 while read x
 do
